3cd13d9c7566775ebe14ecbe8b5c0f10941e263a
Maia Araújo <jessicamaiaaraujo@gmail.com>
2024-03-19 14:41:30 -0300

    pages cadastro e login

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function registrarSentimento() {
-
-    window.location.href = "notas.html";
+function registrarSentimento(tipo) {
+    if (tipo === "login") {
+        window.location.href = "peso.html"; 
+    } else if (tipo === "registro") {
+        window.location.href = "login.html"; 
+    }else if (tipo === "notas") {
+        window.location.href = "notas.html"; 
+    }
 }
